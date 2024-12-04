e2445e9f4617628117fc35499e997d88d46cc654
JavierHernandez115 <hernandezmartinezjavier115@gmail.com>
2024-12-03 21:07:49 -0600

    routing de vacaciones

---
+++ src/app/features/vacaciones/vacaciones-routing.module.ts
@@ -0,0 +1,20 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { FormComponent } from './form/form.component';
+import path from 'path';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    {path: 'lista', component: ListComponent},
+    {path: 'detalles', component: DetailComponent},
+    {path: 'form', component: FormComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  export class EmpleadosRoutingModule { }
+  
