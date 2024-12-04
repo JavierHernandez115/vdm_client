490f5766455c5fcac26c3d6f8101d1e72db44fdd
JavierHernandez115 <hernandezmartinezjavier115@gmail.com>
2024-12-03 22:53:28 -0600

    asistencias terminado

--- src/app/features/asistencias/lista/lista.component.ts
+++ src/app/features/asistencias/lista/lista.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AsistenciasService } from '../service/asistencias.service';
+import { catchError, throwError } from 'rxjs';
 @Component({
   selector: 'app-lista',
   standalone: false,
@@ -8,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './lista.component.css'
 })
 export class ListaComponent {
+  data: any=[];
+  constructor(private apiService: AsistenciasService){}
+  ngOnInit():void{
+    this.ListarEmpleados();
+  }
 
+  ListarEmpleados() {
+    this.apiService.getAll().pipe(
+        catchError((error) => {
+            console.error('Error capturado:', error);
+            return throwError(error);
+        })
+    ).subscribe(
+        (data) => {
+            console.log('Datos recibidos:', data);
+            this.data = data;
+        },
+        (error) => {
+            console.error('Error en el bloque subscribe:', error);
+        }
+    );
+}
 }
--- src/app/features/asistencias/service/asistencias.service.ts
+++ src/app/features/asistencias/service/asistencias.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../enviroments/enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsistenciasService {
+  private BaseUrl= environment.ServUrl+"asistencias"
 
-  constructor() { }
+   //Lista a todas las asistencias
+   getAll(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.BaseUrl}`);
+  }
+
+  //Busca asistencia por Id
+  getById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.BaseUrl}/${id}`);
+  }
+
+   // Actualiza una asistencia
+  update(id: number, empleado: any): Observable<any> {
+    return this.http.put<any>(`${this.BaseUrl}/${id}`, empleado);
+  }
+
+    // Crea un nueva asistencia
+  create(empleado: any): Observable<any> {
+    return this.http.post<any>(`${this.BaseUrl}`, empleado);
+  }
+  
+
+  // Elimina una asistencia
+  delete(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.BaseUrl}/${id}`);
+  }
+  constructor(private http:HttpClient) { }
 }
